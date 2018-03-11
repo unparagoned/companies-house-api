f030e1af2fad80ca83e22a3acf986d26a9b1f0bd
unparagoned <unparagoned@msn.com>
2018-03-11 22:12:29 +0000

    refactored broken

--- companies.py
+++ companies.py
@@ -6,6 +6,8 @@ Set up to download files from companies house
 import requests
 import time
 import json
+import re
+import sys
 from passwords import AUTH_TOKEN
 
 URI_BASE = 'https://api.companieshouse.gov.uk'
@@ -13,13 +15,14 @@ TOKEN = AUTH_TOKEN
 RETRIES = 2
 # should index be 0?
 index = 1
-batch_size = 10 
-total = 40
-page_total = total / batch_size
+BATCH_SIZE_DEFAULT = 10 
+TOTAL_DEFAULT = 40
+SEARCH_DEFAULT = 'dog'
+
 array = "" 
 full_details = []
 
-def apiGet(uri, search_term = None, number_of_items = None, index_offset = None):
+def apiCall(uri, search_term = None, number_of_items = None, index_offset = None):
 	"""
 	This calls the companies house api
 	It retries up to "RETRIES" times if there is an error.
@@ -28,70 +31,98 @@ def apiGet(uri, search_term = None, number_of_items = None, index_offset = None)
 	if (search_term is not None) or (number_of_items is not None) or (index_offset is not None):
 		params = (
 			('q', search_term),
-			('items_per_page', batch_size),
+			('items_per_page', BATCH_SIZE_DEFAULT),
 			('start_index', index_offset)
 		)
 	while error_count < RETRIES:
 
 		response = requests.get(uri, params=params, auth=(TOKEN, ''))
-
+		#if error response 400, 404, etc.
 		if "4" in response:
 			print("Error waiting 5 min before retry")
 			time.sleep( 5*60 )
 			error_count += 1
 		else:
 			#good response
-			return response.json()
+			return response
 	
 	raise ValueError('Unable to make api call: ' + response)
-	#don#t actually want program to continue should quit
-	#return response
+	#don't actually want program to continue should quit
 
+def main():
+	"""
 
-for page in range(0,page_total):
-	index = page * batch_size
+	"""
+	args = sys.argv
+	try:
+		total = re.match(r'.*\-n([^\- ]*).*', args).group(1)
+	except:
+		total = TOTAL_DEFAULT
+		pass
+	try:
+		search = re.match(r'.*\-s([^\- ]*).*', args).group(1)
+	except:
+		search = SEARCH_DEFAULT
+		pass
 	
-	params = (
-		('q', 'dog'),
-		('items_per_page', batch_size),
-		('start_index', index)
-	)
-	response = requests.get('https://api.companieshouse.gov.uk/search', params=params, auth=(TOKEN, ''))
+	try:
 
-	if "4" in response:
-		time.sleep( 5*60 )
-		print("Error")
-	else:
-		array=array +response.text
-		jsoncomp=response.json()
-		companies = jsoncomp['items']
+		page_total = total / BATCH_SIZE_DEFAULT
+
+		response = apiCall(URI_BASE + '/search', search, total, 1)
 		
-		for company in companies:
-			print company['company_number']
-			filing_history = requests.get('https://api.companieshouse.gov.uk/company/'+company['company_number']+'/filing-history', auth=(TOKEN,'')).json()
-			print filing_history
-			company_files =[company, filing_history]
-			for document in filing_history['items']:
-				#print document['links']['document_metadata']
-				document_content = requests.get(document['links']['document_metadata']+'/content', auth=(TOKEN,''))
-				#print document_content.text
-				company_files.append(document_content)
-				company_files.append(document_content.content)
-				f = open(company['company_number'] + document['transaction_id'], "w")
-				f.write(document_content.content)
-			f.close()
+		for page in range(0, page_total):
+			index = page * BATCH_SIZE_DEFAULT
+			
+			params = (
+				('q', search),
+				('items_per_page', BATCH_SIZE_DEFAULT),
+				('start_index', index)
+			)
+			response = requests.get('https://api.companieshouse.gov.uk/search', params=params, auth=(TOKEN, ''))
 
-			full_details.append(company_files)							
+			if "4" in response:
+				time.sleep( 5*60 )
+				print("Error")
+			else:
+				array = array +response.text
+				jsoncomp=response.json()
+				companies = jsoncomp['items']
 				
+				for company in companies:
+					print company['company_number']
+					filing_history = requests.get('https://api.companieshouse.gov.uk/company/'+company['company_number']+'/filing-history', auth=(TOKEN,'')).json()
+					print filing_history
+					company_files =[company, filing_history]
+					for document in filing_history['items']:
+						#print document['links']['document_metadata']
+						document_content = requests.get(document['links']['document_metadata']+'/content', auth=(TOKEN,''))
+						#print document_content.text
+						company_files.append(document_content)
+						company_files.append(document_content.content)
+						f = open(company['company_number'] + document['transaction_id'], "w")
+						f.write(document_content.content)
+					f.close()
+
+					full_details.append(company_files)							
+						
 
 
-#print array
+		#print array
 
-file = open("dogs.txt", "w")
-file.write(full_details)
-file.close()
+		file = open("dogs.txt", "w")
+		file.write(full_details)
+		file.close()
+
+	except:
+		pass
+	finally:
+		return 1
 
 
-exit
+if __name__ == '__main__':
+	main()
+
+
 	
 
